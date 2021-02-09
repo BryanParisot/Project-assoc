328b20683ef130a887c2c60275a71e90c8bffbab
bryanparisot <bryanparisot@hotmail.com>
2021-02-09 14:23:15 +0100

    add bdd in sql

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -17,8 +17,144 @@ class Article
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $race;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descriptif;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $grand_descriptif;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Entente;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getRace(): ?string
+    {
+        return $this->race;
+    }
+
+    public function setRace(string $race): self
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->descriptif;
+    }
+
+    public function setDescriptif(string $descriptif): self
+    {
+        $this->descriptif = $descriptif;
+
+        return $this;
+    }
+
+    public function getGrandDescriptif(): ?string
+    {
+        return $this->grand_descriptif;
+    }
+
+    public function setGrandDescriptif(string $grand_descriptif): self
+    {
+        $this->grand_descriptif = $grand_descriptif;
+
+        return $this;
+    }
+
+    public function getEntente(): ?string
+    {
+        return $this->Entente;
+    }
+
+    public function setEntente(string $Entente): self
+    {
+        $this->Entente = $Entente;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
     }
 }
