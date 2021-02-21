93c9372a030083fdcb122bfe218e03f56cff0e40
bryanparisot <bryanparisot@hotmail.com>
2021-02-21 09:08:35 +0100

    add modified password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationType;
+use App\Form\EditProfilType;
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
@@ -34,7 +35,9 @@ class UserController extends AbstractController
     public function editProfile(Request $request){
 
         $user = $this->getUser();
-        $form = $this->createForm(RegistrationType::class, $user);
+
+        //Je vais récupérer un formulaire user sans les input mdp
+        $form = $this->createForm(EditProfilType::class, $user);
 
         $form->handleRequest($request);
 
@@ -43,6 +46,7 @@ class UserController extends AbstractController
             $em->persist($user);
             $em->flush();
 
+            $this->addFlash('message', 'Profil mis à jour');
             return $this->redirectToRoute('user');
         }
 
@@ -53,4 +57,37 @@ class UserController extends AbstractController
         ]);
 
     }
+
+        /**
+     * @Route("/inscription/edit/password", name="user_edit_password")
+     */
+    public function editPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder){
+
+        if ($request->isMethod('POST')) {
+            # code...
+        
+        $em =$this->getDoctrine()->getManager();
+
+        $user = $this->getUser();
+
+        //on vérifie si les 2 mots de passe sont identiques
+        if ($request->request->get('pass') == $request->request->get('pass1')) {
+
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass1')));
+            $em->flush();
+            $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+            return $this->redirectToRoute('user');
+            
+        }else{
+            $this->addFlash('error', 'les deux mots de passe ne sont pas identiques');
+        }
+    }
+
+        return $this->render('security/editPassword.html.twig', [
+            'title' => 'Modifier votre mot de passe',
+        ]);
+    
+    }
+
 }
